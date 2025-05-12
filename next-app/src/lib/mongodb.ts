980c3e4c210d24d12d47283ac07cab01ce77ccc7
joseph-allen <joseph.allen.1994@hotmail.com>
2025-05-12 22:21:40 +0100

    Fix linting issue

--- next-app/src/lib/mongodb.ts
+++ next-app/src/lib/mongodb.ts
@@ -1,9 +1,13 @@
 import { MongoClient } from "mongodb";
 
+declare global {
+  let _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
 const uri = process.env.MONGODB_URI!;
 const options = {};
 
-let client;
+let client: MongoClient;
 
 if (!global._mongoClientPromise) {
   client = new MongoClient(uri, options);
