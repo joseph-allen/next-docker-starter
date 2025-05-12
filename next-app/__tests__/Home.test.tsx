47096cc7b4336457c21ffdde00e6c6b6f9004aad
joseph-allen <joseph.allen.1994@hotmail.com>
2025-05-12 23:36:26 +0100

    Test husky

--- next-app/__tests__/Home.test.tsx
+++ next-app/__tests__/Home.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 
 describe('Home', () => {
   it('renders a heading', () => {
-    render(<h1>Welcome</h1>);
-    expect(screen.getByText('Welcome')).toBeInTheDocument();
+    render(<h1>Welcome!</h1>);
+    expect(screen.getByText('Welcome!')).toBeInTheDocument();
   });
 });
