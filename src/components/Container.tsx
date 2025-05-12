50a6f3407c176682acae5a68a490480b0b59e2ba
joseph-allen <joseph.allen.1994@hotmail.com>
2025-05-12 17:20:57 +0100

    Add Emotion, and dummy component examples on Emotion use

---
+++ src/components/Container.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh; // full viewport height
+  flex-direction: column;
+  text-align: center;
+  padding: 2rem;
+`;
+
+export default Container;
